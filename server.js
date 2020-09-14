ce544c2713e0ab7d5550a2282be0d3f4b8544673
khaledaylii <khaled.ali@mail.concordia.ca>
2020-09-14 14:45:32 -0400

    feat: add dependency mgmt files

--- server.js
+++ server.js
@@ -1,19 +1,26 @@
-const express = require("express");
-const mongoose = require("mongoose");
-const morgan = require("morgan");
-const path = require("path");
+const express = require('express');
+const mongoose = require('mongoose');
+const morgan = require('morgan');
+const path = require('path');
 
 const app = express();
 const PORT = process.env.PORT || 8080;
 
-app.use(morgan("tiny"));
+const routes = require('./routes/api');
 
-app.get("/post", (req, res) => {
-  const data = {
-    title: "hello world",
-    content: "its me, lorem ipsum sit amet dolor",
-  };
-  res.json(data);
+const MONGODB_URI =
+    'mongodb+srv://admin:twistandshout@cluster0.haguo.mongodb.net/test?retryWrites=true&w=majority';
+
+mongoose.connect('mongodb://localhost/hammeringkeys-main', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
 });
+
+mongoose.connection.on('connected', () => {
+    console.log('Mongoose is connected!');
+});
+
+app.use(morgan('tiny'));
+app.use('/', routes);
 
 app.listen(PORT, console.log(`Server is starting at ${PORT}`));
