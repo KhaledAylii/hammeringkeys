7a37e5421e1b0c898a571fe885dcf2633ef8c9fb
khaledaylii <khaled.ali@mail.concordia.ca>
2020-09-14 19:54:03 -0400

    feat: create navbar

---
+++ client/src/components/navbar/Navbar.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Box, Typography } from '@material-ui/core';
+import styled from 'styled-components';
+import * as styles from '../../App.scss';
+
+const NavbarContainer = styled(Box)`
+    height: 100vh;
+    width: 20%;
+    background-color: ${styles.colorSecondary};
+    color: ${styles.colorTernary};
+    position: fixed;
+    box-shadow: 0px 5px 20px -5px #000;
+    padding: 10px;
+`;
+
+export const Navbar: React.FC = () => {
+    return (
+        <NavbarContainer>
+            <Typography variant="h5" component="h2">
+                <code>hammering keys</code>
+            </Typography>
+        </NavbarContainer>
+    );
+};
