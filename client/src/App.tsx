453c3dd91ec2c1dddde44a679a43a60a677e90c5
khaledaylii <khaled.ali@mail.concordia.ca>
2020-09-14 19:55:08 -0400

    feat: create app structure with navbar

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,22 +1,33 @@
 import React, { Suspense } from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { Box } from '@material-ui/core';
 import LoadingPage from './pages/loading';
 import './App.scss';
 
 const CreatePostPage = React.lazy(() => import('./pages/create-post'));
+const Navbar = React.lazy(() => import('./components/navbar'));
 
 const App: React.FC = () => {
     return (
         <Router>
             <div className="App">
-                <Switch>
-                    <Route path="/posts/create">
+                <Box display="flex">
+                    <Box>
                         <Suspense fallback={LoadingPage}>
-                            <CreatePostPage />
+                            <Navbar />
                         </Suspense>
-                    </Route>
-                    <Route path="/"></Route>
-                </Switch>
+                    </Box>
+                    <Box>
+                        <Switch>
+                            <Route path="/posts/create">
+                                <Suspense fallback={LoadingPage}>
+                                    <CreatePostPage />
+                                </Suspense>
+                            </Route>
+                            <Route path="/"></Route>
+                        </Switch>
+                    </Box>
+                </Box>
             </div>
         </Router>
     );
