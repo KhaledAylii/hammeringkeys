a82ad446a7451874431bb4c4046c08e7ddde8c6b
khaledaylii <khaled.ali@mail.concordia.ca>
2020-09-14 19:54:39 -0400

    feat: prepare creatte post page

--- client/src/pages/create-post/CreatePost.tsx
+++ client/src/pages/create-post/CreatePost.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import { Container } from '@material-ui/core';
 import axios from 'axios';
 
 export const CreatePost: React.FC = () => {
     return (
-        <>
+        <Container className="heyo" style={{ height: '1000px' }}>
             <code>Hey</code>
-        </>
+        </Container>
     );
 };
