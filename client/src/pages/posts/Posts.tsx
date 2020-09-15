aa02fc0ec5cf03e49233ccdaa533cdf5e1e549da
khaledaylii <khaled.ali@mail.concordia.ca>
2020-09-14 20:03:16 -0400

    feat: create post page files

---
+++ client/src/pages/posts/Posts.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import { Container } from '@material-ui/core';
+import axios from 'axios';
+
+export const Posts: React.FC = () => {
+    return <Container></Container>;
+};
