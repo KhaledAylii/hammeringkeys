c2519b3b2d157a533b271eb97e3431dd54e099a4
khaledaylii <khaled.ali@mail.concordia.ca>
2020-09-14 22:25:27 -0400

    fix: add app layout and lazy loading spinner

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,30 +3,46 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import { Box } from '@material-ui/core';
 import LoadingPage from './pages/loading';
 import './App.scss';
+import styled from 'styled-components';
 
 const CreatePostPage = React.lazy(() => import('./pages/create-post'));
+const PostsPage = React.lazy(() => import('./pages/posts'));
 const Navbar = React.lazy(() => import('./components/navbar'));
+
+const NavBarContainer = styled(Box)`
+    width: calc(20% - 20px);
+    max-width: 280px;
+`;
+
+const AppContainer = styled(Box)`
+    width: 80%;
+`;
 
 const App: React.FC = () => {
     return (
         <Router>
             <div className="App">
                 <Box display="flex">
-                    <Box>
-                        <Suspense fallback={LoadingPage}>
+                    <NavBarContainer>
+                        <Suspense fallback={<LoadingPage />}>
                             <Navbar />
                         </Suspense>
-                    </Box>
-                    <Box>
+                    </NavBarContainer>
+                    <AppContainer>
                         <Switch>
                             <Route path="/posts/create">
-                                <Suspense fallback={LoadingPage}>
+                                <Suspense fallback={<LoadingPage />}>
                                     <CreatePostPage />
+                                </Suspense>
+                            </Route>
+                            <Route path="/posts">
+                                <Suspense fallback={LoadingPage}>
+                                    <PostsPage />
                                 </Suspense>
                             </Route>
                             <Route path="/"></Route>
                         </Switch>
-                    </Box>
+                    </AppContainer>
                 </Box>
             </div>
         </Router>
