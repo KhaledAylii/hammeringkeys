e9c3e528908b4f3529f0d54b16737d7de0434371
khaledaylii <khaled.ali@mail.concordia.ca>
2020-09-14 22:22:26 -0400

    feat: get posts and populate posts page

--- client/src/pages/posts/Posts.tsx
+++ client/src/pages/posts/Posts.tsx
@@ -1,12 +1,48 @@
-import React from 'react';
-import { Container } from '@material-ui/core';
+import React, { useState, useEffect } from 'react';
+import { Box } from '@material-ui/core';
 import axios from 'axios';
 import Post from '../../components/post';
+import styled from 'styled-components';
+import Loading from '../loading';
+
+const PostsContainer = styled(Box)`
+    padding: 20px 50px;
+`;
 
 export const Posts: React.FC = () => {
+    const [posts, setPosts] = useState<Array<any>>([]);
+    const [loading, setLoading] = useState<boolean>(true);
+
+    useEffect(() => {
+        axios
+            .get('/api/posts')
+            .then((data) => {
+                setPosts(data?.data);
+                setLoading(false);
+            })
+            .catch((err) => console.log(err));
+    }, []);
+
     return (
-        <Container>
-            <Post />
-        </Container>
+        <>
+            {loading && <Loading />}
+            {!loading && (
+                <PostsContainer display="flex" justifyContent="center">
+                    <Box
+                        display="flex"
+                        justifyContent="flex-start"
+                        flexWrap="wrap"
+                    >
+                        {posts.map((post) => (
+                            <Post
+                                title={post.title}
+                                body={post.body}
+                                thumbnailURL={post.thumbnailURL}
+                            />
+                        ))}
+                    </Box>
+                </PostsContainer>
+            )}
+        </>
     );
 };
